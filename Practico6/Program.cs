d1a1b61b6cd746739cb8760c5165eeb768291d80
bara1422 <xxbaraxx@hotmail.com>
2024-05-25 13:25:01 -0300

    medio practico 6

---
+++ Practico6/Program.cs
@@ -0,0 +1,200 @@
+﻿namespace Practico6
+{
+    internal class Program
+    {
+        static int NumeroValido(string mensaje)
+        {
+            int numero;
+            do
+            {
+                Console.WriteLine(mensaje);
+
+            } while (!int.TryParse(Console.ReadLine(), out numero));
+            return numero;
+        }
+
+
+        static void Ejercicio1()
+        {
+            Console.WriteLine("generar 10 numeros aleatorios entre 1 y 20 incluidos");
+            Console.WriteLine("pedir al usuario numero, si esta en el array mostrar la posicion, sino mostrar no se encontro");
+
+            int[] numeros = new int[20];
+            Random aleatorio = new Random();
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = aleatorio.Next(1, 21);
+            }
+
+            int numeroElegido = NumeroValido("Ingrese un numero para buscar");
+            bool numeroEstaEnArray = false;
+            //  for(int i = 0; i < numeros.Length; i++)
+            //  {
+            //      if (numeros[i] == numeroElegido)
+            //      {
+            //          numeroEstaEnArray = true;
+            //      }
+            //  }
+
+            int contador = 0;
+            while (contador < numeros.Length)
+            {
+                if (numeros[contador] == numeroElegido)
+                {
+                    Console.WriteLine($"El numero {numeroElegido} esta en la posicion {contador}");
+                    numeroEstaEnArray = true;
+                }
+                contador++;
+            }
+            if (!numeroEstaEnArray)
+            {
+                Console.WriteLine($"El numero {numeroElegido} no se encontro");
+            }
+        }
+
+        static void Ejercicio2()
+        {
+            Console.WriteLine("Generar array[8] llenar con num aleat.");
+            Console.WriteLine("Crear otro de[8] llenar con los mismos numero pero inverso orden y mostrarlos en pantalla");
+
+            int[] arrOriginal = new int[8];
+            int[] arrInverso = new int[8];
+
+            int numero;
+            Random aleatorio = new Random();
+
+
+            for (int i = 0; i < arrOriginal.Length; i++)
+            {
+                arrOriginal[i] = aleatorio.Next(0, 101);
+            }
+
+            int contador = 0;
+            for (int i = arrInverso.Length - 1; i >= 0; i--)
+            {
+                arrInverso[i] = arrOriginal[contador];
+                contador++;
+            }
+
+            foreach (int i in arrOriginal)
+            {
+
+                Console.Write($"{i} \t ");
+            }
+            Console.WriteLine();
+            foreach (int i in arrInverso)
+            {
+                Console.Write($"{i} \t");
+            }
+        }
+
+        static void Ejercicio3()
+        {
+            Console.WriteLine("generar dos arr[5] con numeros al entre 3 y 57. crear un 3er arr[5] con suma de los dos arr");
+
+            int[] arr1 = new int[5];
+            int[] arr2 = new int[5];
+            int[] arr3 = new int[5];
+            Random random = new Random();
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = random.Next(3, 57);
+                arr2[i] = random.Next(3, 57);
+            }
+            for (int i = 0; i < arr3.Length; i++)
+            {
+                arr3[i] = arr2[i] + arr1[i];
+            }
+
+            foreach (int i in arr1)
+            {
+                Console.Write($"{i} \t");
+            }
+            Console.WriteLine();
+            foreach (int i in arr2)
+            {
+                Console.Write($"{i} \t");
+
+            }
+            Console.WriteLine();
+            foreach (int i in arr3)
+            {
+                Console.Write($"{i} \t");
+            }
+        }
+
+        static int CantAlumnos()
+        {
+            int cantAlumnos;
+            do
+            {
+                Console.WriteLine("Ingrese cantidad alumnos");
+            } while (!int.TryParse(Console.ReadLine(), out cantAlumnos));
+            return cantAlumnos;
+        }
+
+        static double NotaValida(string mensaje)
+        {
+            double notaValida;
+            do
+            {
+                Console.Write(mensaje);
+            }while(!double.TryParse(Console.ReadLine(), out notaValida));
+            return notaValida;
+        }
+        
+
+      
+
+        static void Ejercicio4()
+        {
+            Console.WriteLine("cargar notas examenes, ingresar por teclado cant alumn, luego cargar notas en arr, buscar nota mas alta y mostrarla, indicar indice de arr");
+            int cantAlumnos = CantAlumnos();
+            double[] notasAlumnos = new double[cantAlumnos];
+
+            for (int i = 0; i < notasAlumnos.Length; i++)
+            {
+                notasAlumnos[i] = NotaValida($"Ingrese la {i + 1} nota: ");
+            }
+
+            double notaMasAlta = notasAlumnos[0];
+            // foreach(int i in notasAlumnos)
+            // {
+            //     if (notasAlumnos[i] > notaMasAlta)
+            //     {
+            //         notaMasAlta = notasAlumnos[i];
+            //     }
+            // }
+
+            int indiceNotaMasAlta = 0;
+            for(int i = 0; i < notasAlumnos.Length;i++)
+            {
+                if (notasAlumnos[i] > notaMasAlta)
+                {
+                    indiceNotaMasAlta = i;
+                    notaMasAlta = notasAlumnos[i];
+                }
+            }
+            Console.WriteLine($"La nota mas alta es {notaMasAlta} y esta en el indice {indiceNotaMasAlta}");
+        }
+
+        static void Ejercicio5()
+        {
+            Console.WriteLine("Ingresar numero, validad numero, arr separar digitos, reccorrer arr y mostrar el indicia que contiene digito mayor");
+            int numero;
+            do
+            {
+                Console.WriteLine("Ingrese un numero");
+            }while(!int.TryParse(Console.ReadLine(), out numero));
+            //int cantDigitosNumero = numero.Split("");
+            int[] arrDigitos = new int[numero];
+
+        }
+
+        static void Main(string[] args)
+        {
+            Ejercicio4();
+        }
+    }
+}
---
+++ WriteLine/Program.cs
@@ -0,0 +1,126 @@
+﻿namespace WriteLine
+{
+    internal class Program
+    {
+        public static string archivo = "test2025.txt";
+        public struct Alumno
+        {
+            public int Legajo;
+            public string Nombre;
+            public string Apellido;
+        }
+
+        public static Alumno LeerAlumno()
+        {
+            Alumno alumno = new Alumno();
+            Console.WriteLine("Ingrese legajo");
+            string legajo = Console.ReadLine();
+            while (!int.TryParse(legajo, out alumno.Legajo))
+            {
+                Console.WriteLine("No ingreso un numero entero, Ingrese un numero de legajo:");
+                legajo = Console.ReadLine();
+            }
+            Console.WriteLine("Ingrese nombre");
+            alumno.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese apellido");
+            alumno.Apellido = Console.ReadLine();
+
+            return alumno;
+        }
+
+        public static void EditarAlumno(Alumno alumno)
+        {
+            List<Alumno> alumnos = RetornarListaAlumnos(archivo);
+            if(alumnos.Any(a => a.Legajo == alumno.Legajo))
+            {
+                for(int i = 0; i< alumnos.Count; i++)
+                {
+                    if (alumnos[i].Legajo == alumno.Legajo)
+                    {
+                        Alumno alu = alumnos[i];
+                        alu.Nombre = alumno.Nombre;
+                        alu.Apellido = alumno.Apellido;
+                    }
+                }
+                EscribirAlumno(alumnos, false);
+            }
+        }
+
+        public static void EliminarAlumno(int Legajo)
+        {
+            List<Alumno> alumnos = RetornarListaAlumnos(archivo);
+            bool existe = false;
+            Alumno borrar = new Alumno();
+            foreach (Alumno alumno in alumnos)
+            {
+                if (alumno.Legajo == Legajo)
+                {
+                    borrar = alumno;
+                    existe = true;
+                }
+            }
+            if (existe)
+            {
+                alumnos.Remove(borrar);
+            }
+            EscribirAlumno(alumnos, false);
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            Alumno alumno = LeerAlumno();
+            List<Alumno> listAlumno = new List<Alumno>();
+            listAlumno.Add(alumno);
+            EscribirAlumno(listAlumno, true);
+            //List<Alumno> ListaAlumnos = new List<Alumno>();
+            List<Alumno> listaAlumno = RetornarListaAlumnos(archivo);
+            Alumno alumnoEditar = new Alumno();
+            alumnoEditar.Nombre = "Ricardo";
+            alumnoEditar.Apellido = "Stickar";
+            alumnoEditar.Legajo = 1;
+
+            foreach (Alumno alumnoEnLista in listaAlumno)
+            {
+                Console.WriteLine(alumnoEnLista.Legajo + ", " + alumnoEnLista.Nombre + ", " + alumnoEnLista.Apellido);
+            }
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static List<Alumno> RetornarListaAlumnos(string archivo)
+        {
+            List<Alumno> listaAlumnos = new List<Alumno>();
+            using (StreamReader sr = new StreamReader(archivo))
+            {
+
+                string? linea = sr.ReadLine();
+
+                while (linea != null)
+                {
+                    string[] alumnoArchivo = linea.Split(',');
+                    Alumno alumnoStruct = new Alumno();
+                    alumnoStruct.Legajo = int.Parse(alumnoArchivo[0]);
+                    alumnoStruct.Nombre = alumnoArchivo[1];
+                    alumnoStruct.Apellido = alumnoArchivo[2];
+                    listaAlumnos.Add(alumnoStruct);
+                    //ListaAlumnos.OrderBy( x => x.Apellido ).ToList(); ordenar por apellido
+                    linea = sr.ReadLine();
+                }
+            }
+            return listaAlumnos;
+        }
+
+        private static void EscribirAlumno(List<Alumno> alumnos, bool concatenar)
+        {
+            using (StreamWriter escritor = new StreamWriter(archivo, concatenar))
+            {
+                foreach (Alumno alumno in alumnos)
+                {
+                    escritor.WriteLine(alumno.Legajo + ", " + alumno.Nombre + ", " + alumno.Apellido);
+                }
+            }
+        }
+    }
+}
+}
